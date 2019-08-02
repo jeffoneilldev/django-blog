8acbe9b56abdcf4994c27680cdd0193552b48094
Ubuntu <ubuntu@ip-172-31-85-192.ec2.internal>
2019-08-02 14:54:39 +0000

    Added Heroku and Postgres support for deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('SECRET_KEY', 'jz=@+v+u6u#d+^n2!uj+r9v^a7@hs*y1203oiqlq8^1q!@2q!!')
+
+os.environ.setdefault('DATABASE_URL', 'postgres://nhpcyglirtglem:a58fec523c1c4cb49768f0e51985445a9e76452697ac9676679af8bccb71924a@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/ddmo6e0s9la212')
